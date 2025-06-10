06088f8c02e5dfa38d56bd1db44cda034f6e1b7e
nkknj <p8580393@gmail.com>
2025-06-10 15:33:37 +0900

    Create simple_correction-stack.ts

---
+++ lib/simple_correction-stack.ts
@@ -0,0 +1,63 @@
+import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as path from 'path';
+
+export class SimpleCorrectionStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // 1. フロントエンド用 S3 バケットとデプロイ
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '..', 'frontend', 'build'))],
+      destinationBucket: websiteBucket,
+    });
+
+    // CloudFront
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: { origin: new origins.S3Origin(websiteBucket) },
+    });
+
+    // 2. バックエンド Lambda 関数
+    const correctionLambda = new lambda.Function(this, 'CorrectionFunction', {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      handler: 'index.lambda_handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '..', 'lambda')),
+      timeout: Duration.seconds(60),
+      environment: {
+        MODEL_ID: 'us.amazon.nova-lite-v1:0'
+      },
+    });
+
+    // 3. API Gateway
+    const api = new apigateway.LambdaRestApi(this, 'ApiGateway', {
+      handler: correctionLambda,
+      proxy: false,
+      defaultCorsPreflightOptions: {
+        allowOrigins: ['*'],
+        allowMethods: ['POST', 'OPTIONS'],
+      },
+    });
+    const process = api.root.addResource('process');
+    process.addMethod('POST');
+
+    // 4. 出力
+    new cdk.CfnOutput(this, 'WebsiteURL', {
+      value: `https://${distribution.distributionDomainName}`
+    });
+    new cdk.CfnOutput(this, 'ApiURL', {
+      value: `${api.url}process`
+    });
+  }
+}
